d2fed939fb7aee3abaa865dfe720335b8defaf57
nkmisc88-jpg <nkmisc88@gmail.com>
2026-09-23 00:49:38 +0530

    Update build.gradle.kts

--- MeowTVProvider/build.gradle.kts
+++ MeowTVProvider/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     description = "MeowTV provider"
     authors = listOf("nkmisc88-jpg")
-    status = 3
+    status = 1
     tvTypes = listOf("Movie", "TvSeries", "Anime")
     language = "en"
     iconUrl = "https://www.google.com/s2/favicons?domain=meowtv.ru&sz=%size%"
